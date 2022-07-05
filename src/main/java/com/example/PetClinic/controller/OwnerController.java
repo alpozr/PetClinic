30258ee134f09395cd31c765414481d84f1de8ff
iniesta1 <66780692+iniesta1@users.noreply.github.com>
2022-07-06 01:43:32 +0300

    Classes committed.

--- src/main/java/com/example/PetClinic/controller/OwnerController.java
+++ src/main/java/com/example/PetClinic/controller/OwnerController.java
@@ -14,24 +14,26 @@ import java.util.List;
 @RequiredArgsConstructor
 public class OwnerController {
 
-        private final OwnerService ownerService;
-        @PostMapping("/save")
-        public ResponseEntity<OwnerDto> save(@RequestBody OwnerDto ownerDto){
-            return ResponseEntity.ok(ownerService.save(ownerDto));
-        }
-        @GetMapping()
-          public ResponseEntity<List<OwnerDto>> getAll(){
-          return ResponseEntity.ok(ownerService.getAll());
+    private final OwnerService ownerService;
+
+    @PostMapping("/save")
+    public ResponseEntity<OwnerDto> save(@RequestBody OwnerDto ownerDto) {
+        return ResponseEntity.ok(ownerService.save(ownerDto));
     }
+
+    @GetMapping()
+    public ResponseEntity<List<OwnerDto>> getAll() {
+        return ResponseEntity.ok(ownerService.getAll());
+    }
+
     @GetMapping("/delete/{id}")
-    public ResponseEntity<String> delete(@PathVariable Long id ){
-            try {
-                ownerService.delete(id);
-                return ResponseEntity.ok("Owner has been deleted");
-            }
-            catch(Exception e) {
-                return ResponseEntity.ok("Couldnt find id,please write correct id!");
-            }
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        try {
+            ownerService.delete(id);
+            return ResponseEntity.ok("Owner has been deleted");
+        } catch (Exception e) {
+            return ResponseEntity.ok("Couldnt find id,please write correct id!");
+        }
     }
 
 
